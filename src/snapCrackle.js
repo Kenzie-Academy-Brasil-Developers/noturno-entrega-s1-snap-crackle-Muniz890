bea03c8313cbfb8f1804a69abc25f0047b11efdf
Muniz890 <91911992+Muniz890@users.noreply.github.com>
2021-11-09 22:20:23 -0300

    entrega: SnapCrackle

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,21 @@
-// Seu código vai aqui
+function snapCrackle(maxValue) {
+    let text = "";
+    for (let value = 1; value <= maxValue; value++) {
+        if (value % 2 !== 0 && value % 5 === 0) {
+            text += "SnapCrackle, ";
+            continue;
+        }
+        if (value % 2 !== 0) {
+            text += "Snap, ";
+            continue;
+        }
+        if (value % 5 === 0) {
+            text += "Crackle, ";
+            continue;
+        }
+        text += value + ", ";
+    }
+    console.log(text);
+}
+
+snapCrackle(15);
